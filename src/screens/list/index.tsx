45e8defd460123f1d0e017c850a720c9228c64fc
Abdulaziz Homaily <a.homaily@gmail.com>
2022-10-04 03:07:36 +0300

    Add screen list items Add typography component Add avatar component

--- src/screens/list/index.tsx
+++ src/screens/list/index.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {ScrollView} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+import ListData from '../../utils/fake-data';
+import {ListItem} from './components/item';
 
 //
 //
+
+export interface IListItem {
+  id: string;
+  name: string;
+  description: String;
+  price: string;
+  salePrice: any;
+  brand: String;
+}
 
 const ListScreen = () => {
   return (
-    <View style={{padding: 20}}>
-      <Text>List Screen</Text>
-    </View>
+    <SafeAreaView edges={['top', 'bottom']}>
+      <ScrollView contentContainerStyle={{paddingHorizontal: 16}}>
+        {ListData.map(item => <ListItem key={item.id} item={item} />)}
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
