bae54011aaec6907d5dea58ba5682397a42e60e1
Oleksandr Sokolov <oleksandr.sokolov@litslink.com>
2022-10-06 10:38:38 +0300

    add babel-plugin-module-resolver to use absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,30 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@assets': './assets',
+          '@components': './src/components',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@global-types': './src/types',
+        },
+      },
+    ],
+  ],
 };
